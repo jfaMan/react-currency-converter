83f13f813f33e66cf41ca755f15f553298183de9
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-07-26 17:38:00 +0900

    added node-sass, sass-loader and stylesheet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="header">
-      <h1>Currency Converter</h1>
+      <h1>React Currency Converter</h1>
     </div>
   );
 };
