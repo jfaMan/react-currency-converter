2029c62ae010efe9a2b4039eb0aba45196df5433
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-07-27 22:18:41 +0900

    fixed fetch error

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import React, { useEffect } from 'react';
 import Header from './components/Header';
 import Converter from './components/Converter';
 
-
 const App = () => {
-
   useEffect(() => {
-    fetch('http://api.exchangeratesapi.io/v1/latest?access_key=');
+    fetch('http://api.exchangeratesapi.io/v1/latest?access_key=4f1daa95f33a32d1c3f5a99b3d7c4ecf')
       .then(response => response.json())
-      .then(data => console.log(data))
-  }, [])
+      .then(data => console.log(data.rates));
+  }, []);
 
   return (
     <div>
