2b8dee98ece7885d1c42770743f1a8f88a20b1d9
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-07-27 15:23:14 +0900

    added basic style

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -4,13 +4,52 @@ const Converter = () => {
   return (
     <div className="converter">
       <div>
-        <h2>This is the converter container</h2>
+        <h2>Enter an amount and select your currencies</h2>
+        <div className="container converter-main">
+          <div className="row">
+            <div className="col-sm-5">
+              <label htmlFor="amount">Amount</label>
+            </div>
+            <div className="col-sm-3">
+              <label htmlFor="currency-from">From</label>
+            </div>
+            <div className="col-sm-1">
+            </div>
+            <div className="col-sm-3">
+              <label htmlFor="currency-to">To</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-5">
+              <input className="input" type="text" id="amount"/>
+            </div>
+            <div className="col-sm-3">
+              <select className="input" name="currencies" id="currencies">
+                <option value="AUD">🦘AUD</option>
+              </select>
+            </div>
+            <div className="col-sm-1">
+
+            </div>
+            <div className="col-sm-3">
+              <select className="input" name="currencies" id="currencies">
+                <option value="AUD">🦘AUD</option>
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className="converter-bottom">
+          <div className="results">
+            <h4>Results go here</h4>
+          </div>
+          <button className="btn btn-primary">Convert</button>
+        </div>
       </div>
       <div>
 
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Converter;
