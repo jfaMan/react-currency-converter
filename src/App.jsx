d4118d9609401028083dac34849279a4b4f96566
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-07-27 23:59:23 +0900

    added input amount

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Converter from './components/Converter';
 
 const App = () => {
+  const [inputAmount, setInputAmount] = useState('');
+  const [selectedAmount, setSelectedAmount] = useState('');
+  const [selectedCurrency, setSelectedCurrency] = useState('AUD');
+  const [result, setResult] = useState('');
   useEffect(() => {
     fetch('http://api.exchangeratesapi.io/v1/latest?access_key=4f1daa95f33a32d1c3f5a99b3d7c4ecf')
       .then(response => response.json())
-      .then(data => console.log(data.rates));
+      .then(data => {
+        console.log(data);
+        const rate = data;
+        document.querySelector('input').innerHTML = rate;
+      });
   }, []);
 
   return (
