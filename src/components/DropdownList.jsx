9a5922e91dd516ca53ed811fbea92bfdf1617304
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-07-28 22:32:39 +0900

    added logo

--- src/components/DropdownList.jsx
+++ src/components/DropdownList.jsx
@@ -4,6 +4,7 @@ const DropdownList = (props) => {
   const {currencies, value, id, onChange} = props;
   return (
     <select className="input" name="currencies" value={value} onChange={onChange} id={id}>
+      <option value="">-- Select a currency --</option>
       {currencies.map(currency => <option value={currency} key={currency}>{currency}</option> )}
     </select>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="header">
+      <img src="logo192.png" alt="React Logo" />
       <h1>React Currency Converter</h1>
     </div>
   );
