fc8db5522e4431fe3add495191be5b651670b9fd
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-08-02 23:48:05 +0900

    working on mobile responsiveness

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Header = () => {
   return (
     <div className="header">
-      <div className="header-top">
+      <div className="header-logo">
         <img src="logo192.png" alt="React Logo" />
       </div>
-      <div className="header-right">
+      <div className="header-text">
         <h1>React Currency Converter</h1>
         <p>Check exchange rates with the power of React</p>
       </div>
