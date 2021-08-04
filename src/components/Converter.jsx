068039b927afea59e719a3e3d3ad29e25d1ca1c4
Allan Szewczyk <jfa_nakata@hotmail.com>
2021-08-04 16:26:16 +0900

    fixed margins for results in mobile

--- src/components/Converter.jsx
+++ src/components/Converter.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight,faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import React from 'react';
 import DropdownList from './DropdownList';
 
